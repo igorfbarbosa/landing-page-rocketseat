725515e34d15a9a2b490911c15649a72f28f254e
igorfbarbosa <igorfbarbosa@gmail.com>
2025-05-11 00:29:08 -0300

    Fundamentos do Next.js - Criando o componente Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
+import { Header } from '@/components/header'
 import { Button } from '@/components/ui/button'
 import { Geist, Geist_Mono } from 'next/font/google'
-import Image from 'next/image'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -17,7 +17,7 @@ export default function Home() {
     <div
       className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
-      <Button variant="secondary">Botao</Button>
+      <Header />
     </div>
   )
 }
