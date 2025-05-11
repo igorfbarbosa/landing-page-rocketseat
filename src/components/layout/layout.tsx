dd3a6c90107d5fd23b9a78e58290ff94a4d04fb9
igorfbarbosa <igorfbarbosa@gmail.com>
2025-05-11 01:57:06 -0300

    Fundamentos do Next.js - Adicionando Style Guide

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,3 +1,4 @@
+import { Inter } from 'next/font/google'
 import type { ReactNode } from 'react'
 import { Footer } from '../footer'
 import { Header } from '../header'
@@ -6,9 +7,13 @@ interface LayoutProps {
   children: ReactNode
 }
 
+const inter = Inter({ subsets: ['latin'] })
+
 export function Layout({ children }: LayoutProps) {
   return (
-    <div className="relative flex min-h-screen flex-col dark">
+    <div
+      className={`${inter.className} relative flex min-h-screen flex-col bg-gray-700`}
+    >
       <Header />
       <main className="flex-1 flex flex-col mb-12">{children}</main>
       <Footer />
