1d95f5b10821379b2573a8dc0a35a8a07f79f05f
igorfbarbosa <igorfbarbosa@gmail.com>
2025-05-11 00:50:43 -0300

    Fundamentos do Next.js - Criando o Layout

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from 'react'
+import { Header } from '../header'
+
+interface LayoutProps {
+  children: ReactNode
+}
+
+export function Layout({ children }: LayoutProps) {
+  return (
+    <div className="relative flex min-h-screen flex-col dark">
+      <Header />
+      <main className="flex-1 flex flex-col mb-12">{children}</main>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import { Header } from '@/components/header'
-import { Button } from '@/components/ui/button'
 import { Geist, Geist_Mono } from 'next/font/google'
 
 const geistSans = Geist({
@@ -16,8 +14,6 @@ export default function Home() {
   return (
     <div
       className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <Header />
-    </div>
+    />
   )
 }
