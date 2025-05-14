3acf3bdf4445bcc344bfaf0056078f614dd8ca6f
igorfbarbosa <igorfbarbosa@gmail.com>
2025-05-14 00:17:58 -0300

    Fundamentos do Next.js - Criando o PostCard

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -1,5 +1,6 @@
 import { Search } from '@/components/search'
 import { useRouter } from 'next/router'
+import { PostCard } from './components/post-card'
 
 export function BlogList() {
   const router = useRouter()
@@ -24,6 +25,8 @@ export function BlogList() {
           <Search />
         </div>
       </header>
+
+      <PostCard />
     </div>
   )
 }
