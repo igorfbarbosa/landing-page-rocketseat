477efe971e8ebc3a19275ed1e39fbbe7fce89436
igorfbarbosa <igorfbarbosa@gmail.com>
2025-05-17 07:48:12 -0300

    App Router - Migrando a Listagem de Posts

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,17 +1,18 @@
 import { cn } from '@/lib/utils'
 import { CircleX, SearchIcon } from 'lucide-react'
-import { useRouter } from 'next/router'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { useCallback } from 'react'
 
 export function Search() {
   const router = useRouter()
-  const query = (router.query.q as string) ?? ''
+  const searchParams = useSearchParams()
+  const query = searchParams?.get('q')
 
   const handleSearch = useCallback(
     (event: React.FormEvent) => {
       event.preventDefault()
 
-      if (query.trim()) {
+      if (query?.trim()) {
         router.push(`/blog?q=${encodeURIComponent(query)}`)
       }
     },
@@ -21,15 +22,13 @@ export function Search() {
   function handleQueryChange(event: React.ChangeEvent<HTMLInputElement>) {
     const newQuery = event.target.value
 
-    router.push(`/blog?q=${encodeURIComponent(newQuery)}`, undefined, {
-      shallow: true,
+    router.push(`/blog?q=${encodeURIComponent(newQuery)}`, {
       scroll: false,
     })
   }
 
   function resetSearch() {
-    router.push('/blog', undefined, {
-      shallow: true,
+    router.push('/blog', {
       scroll: false,
     })
   }
