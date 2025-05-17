ea4199743d85a6078a088a810b91a52cdb4f3599
igorfbarbosa <igorfbarbosa@gmail.com>
2025-05-17 09:51:53 -0300

    add seo in blog list page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,28 @@
 import { BlogList } from '@/templates/blog/blog-list'
 import { allPosts } from 'contentlayer/generated'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Blog',
+  description: 'Dicas e estratégias para impulsionar seu negócio',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Blog',
+    description: 'Dicas e estratégias para impulsionar seu negócio',
+    url: 'https://landing-page-rocketseat-seven.vercel.app/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'pt_BR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://landing-page-rocketseat-seven.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set',
+      },
+    ],
+  },
+}
 
 export default function BlogListPage() {
   const sortedPosts = allPosts.sort(
